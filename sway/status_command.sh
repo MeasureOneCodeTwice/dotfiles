0b1c3dfd54e043b294014825a03b8df54f94548b
Logan <logand007@outlook.com>
2026-07-23 19:04:57 -0500

    minor refactoring of status bar script

--- sway/status_command.sh
+++ sway/status_command.sh
@@ -1,11 +1,14 @@
 ### ---- IMPORTANT ---- ###
 # WHEN RUN BY SWAY, THIS SCRIPT CANNOT EXECUTE ANY AUXILARY SCRIPTS!!! #
+
+BATTERY_CAPACITY_FILE=/sys/class/power_supply/BAT0/capacity
+BATTERY_STATUS_FILE=/sys/class/power_supply/BAT0/status
 while true
 do
 	date=$(date +'%b%d %X') 
 	wifi_name=$(nmcli --fields NAME c | head -2 | tail -1 | tr -d '\ ')
-	battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-	battery_status=$(cat /sys/class/power_supply/BAT0/status) 
+	battery_level=$(cat "$BATTERY_CAPACITY_FILE")
+	battery_status=$(cat "$BATTERY_STATUS_FILE") 
 	battery_status=${battery_status:0:1}
 	battery_status=${battery_status,,}
     volume=$(amixer sget 'Master' | tail -1 | \
